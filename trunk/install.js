9eebb3d5772721b779c1d211690741d8aa21020f
kps <kps@20cfa169-23c5-d2f0-5c7e-8cf07d5f3733>
2006-08-04 15:04:36 +0000

    Files for mozilla xpi

---
+++ trunk/install.js
@@ -0,0 +1,11 @@
+var err = initInstall("Litreoir GaelSpell do Mhozilla", "ga-IE@dictionaries.addons.mozilla.org", "4.1");
+if (err != SUCCESS)
+    cancelInstall();
+
+var fProgram = getFolder("Program");
+err = addDirectory("", "ga-IE@dictionaries.addons.mozilla.org",
+		   "dictionaries", fProgram, "dictionaries", true);
+if (err != SUCCESS)
+    cancelInstall();
+
+performInstall();
